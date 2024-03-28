ae83ee9c224c00a6a66bdd93386bdb91c24842dc
Draigon07 <ian.flores2@utp.ac.pa>
2024-03-28 10:52:21 -0500

    testimonials data and compoent done

---
+++ src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Slider from "react-slick";
+import slideSettings from "../../utils/sliderSettings";
+import TestimonialData from "../../utils/testimonialsData";
+
+const Testimonials = () => {
+  return (
+    <div className="py-10 mb-10">
+      <div className="container">
+        <div className="mb-10">
+          <h1
+            data-aos="fade-up"
+            className="text-center text-4xl font-bold font-cursive"
+          >
+            Testimonials
+          </h1>
+        </div>
+        <div data-aos="zoom-in">
+          <Slider {...slideSettings}>
+            {TestimonialData.map((data) => (
+              <div className="my-6" key={data.id}>
+                <div
+                  key={data.id}
+                  className="flex flex-col gap-4 shadow-lg py-8 px-6 mx-4 rounded-xl  bg-primary/10 relative"
+                >
+                  <div className="mb-4">
+                    <img
+                      src={data.img}
+                      alt=""
+                      className="rounded-full w-20 h-20"
+                    />
+                  </div>
+                  <div className="flex flex-col items-center gap-4">
+                    <div className="space-y-3">
+                      <p className="text-xs text-gray-500">{data.text}</p>
+                      <h1 className="text-xl font-bold text-black/80  font-cursive2">
+                        {data.name}
+                      </h1>
+                    </div>
+                  </div>
+                  <p className="text-black/20 text-9xl font-serif absolute top-0 right-0">
+                    ,,
+                  </p>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
---
+++ src/utils/testimonialsData.js
@@ -0,0 +1,30 @@
+const mainURL = "https://api.dicebear.com/8.x/big-smile/svg?seed=";
+const TestimonialData = [
+    {
+      id: 1,
+      name: "Felix",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+      img: `${mainURL}Felix`,
+    },
+    {
+      id: 2,
+      name: "John",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+      img: `${mainURL}John Doe`,
+    },
+    {
+      id: 3,
+      name: "Jane",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+      img: `${mainURL}Jane Doe`,
+    },
+    {
+      id: 4,
+      name: "Aneka",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+      img: `${mainURL}Aneka`,
+    },
+  ];
+
+
+  export default TestimonialData;
