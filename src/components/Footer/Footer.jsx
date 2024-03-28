b248bc5ce172ff84844914ffec8e6128b7768304
Draigon07 <ian.flores2@utp.ac.pa>
2024-03-28 10:56:05 -0500

    footer component done

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa6";
+import FooterBg from "../../assets/website/coffee-footer.jpg";
+import FooterLinks from "../../utils/footerLinks";
+const bgImage = {
+  backgroundImage: `url(${FooterBg})`,
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  minHeight: "400px",
+  width: "100%",
+};
+const Footer = () => {
+  return (
+    <div style={bgImage} className="text-white">
+      <div className="bg-black/40 min-h-[400px]">
+        <div className="container grid md:grid-cols-3 pb-20 pt-5">
+    
+          <div className="py-8 px-4">
+            <a
+              href="#"
+              className="font-semibold tracking-widest text-2xl sm:text-3xl font-cursive
+"
+            >
+              Coffee Cafe
+            </a>
+            <p className="  pt-4">
+              Crafted Coffee, Cozy Vibes, Unforgettable Moments – Your Perfect
+              Espresso Escape
+            </p>
+          </div>
+
+  
+          <div className="col-span-2 grid grid-cols-2 sm:grid-cols-3 md:pl-10">
+            <div className="py-8 px-4">
+              <h1 className="text-xl font-semibold sm:text-left mb-3">
+                Important Links
+              </h1>
+              <ul className="space-y-3">
+                {FooterLinks.map((data, index) => (
+                  <li key={index}>
+                    <a
+                      href={data.link}
+                      className="inline-block hover:scale-105 duration-200"
+                    >
+                      {data.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          
+            <div className="py-8 px-4">
+              <h1 className="text-xl font-semibold sm:text-left mb-3">
+                Quick Links
+              </h1>
+              <ul className="space-y-3">
+                {FooterLinks.map((data, index) => (
+                  <li key={index}>
+                    <a
+                      href={data.link}
+                      className="inline-block hover:scale-105 duration-200 "
+                    >
+                      {data.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="py-8 px-4 col-span-2 sm:col-auto">
+              <h1 className="text-xl font-semibold sm:text-left mb-3">
+                Address
+              </h1>
+              <div>
+                <p className="mb-3">Panama, San Francisco</p>
+                <p>+507 60568927</p>
+
+               
+                <div className="flex items-center gap-3 mt-6">
+                  <a href="https://www.instagram.com/ianm_0407/" target="_blank">
+                    <FaInstagram className="text-3xl hover:text-primary duration-300" />
+                  </a>
+                  <a href="#">
+                    <FaFacebook className="text-3xl hover:text-primary duration-200" />
+                  </a>
+                  <a href="https://www.linkedin.com/in/ian-miguel-31349024a/" target="_blank">
+                    <FaLinkedin className="text-3xl hover:text-primary duration-200" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/utils/footerLinks.js
@@ -0,0 +1,20 @@
+const FooterLinks = [
+    {
+      title: "Home",
+      link: "/#",
+    },
+    {
+      title: "About",
+      link: "/#about",
+    },
+    {
+      title: "Contact",
+      link: "/#contact",
+    },
+    {
+      title: "Blog",
+      link: "/#blog",
+    },
+  ];
+
+  export default FooterLinks
