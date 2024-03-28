ae07785ac641525d2158d56a0509497409b3be70
Draigon07 <ian.flores2@utp.ac.pa>
2024-03-28 10:35:21 -0500

    servicesData added on utils

---
+++ src/utils/serviceData.js
@@ -0,0 +1,25 @@
+const ServicesData = [
+    {
+      id: 1,
+      name: "Espresso",
+      description:
+        "Indulge in the rich and intense flavor of our Espresso. Crafted with precision and passion, each shot is a symphony of bold notes, delivering a perfect pick-me-up for any moment. Savor the essence of pure coffee bliss with every sip",
+      aosDelay: "200",
+    },
+    {
+      id: 2,
+      name: "Americano",
+      description:
+        "Experience the pure essence of coffee with our Americano. A harmonious blend of robust espresso and hot water, delivering a smooth, bold taste that invigorates your senses.",
+      aosDelay: "300",
+    },
+    {
+      id: 3,
+      name: "Cappuccino",
+      description:
+        "Indulge in creamy perfection with our Cappuccino. A harmonious balance of rich espresso, velvety steamed milk, and a delicate layer of frothy foam.",
+      aosDelay: "400",
+    },
+  ];
+
+  export default ServicesData;
